5ef1c877867436d24dccab89e9f01c9c7de2c437
RachelTurekSullivan <Rachel.Sullivan@improving.com>
2021-06-23 08:21:29 -0500

    Add project files.

---
+++ Parks-and-Recreation/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Parks_and_Recreation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Parks-and-Recreation/Services/APIService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Parks_and_Recreation.Models;
+using System.Xml.Serialization;
+
+namespace Parks_and_Recreation.Services
+{
+    public class APIService
+    {
+        private string parkDataURL = "https://seriouslyfundata.azurewebsites.net/api/parks";
+        
+        public async Task<int> GetParkData()
+        {
+
+            var client = new HttpClient();
+            var result = await client.GetAsync(parkDataURL);
+            result.EnsureSuccessStatusCode();
+
+            var resultStr = await result.Content.ReadAsStringAsync();
+            Console.WriteLine(resultStr);
+            return Convert.ToInt32(resultStr);
+        }
+
+        
+    }
+}
